7a47e962f01eeb514457d527454ee3e98ded1bae
Sean Roelofs <sroelofs@stanford.edu>
2019-05-24 21:46:40 -0700

    Updates to util and results helpers

--- src/results.py
+++ src/results.py
@@ -1,42 +1,28 @@
 import numpy as np
 import csv
 from util import read_file, write_file
+import util
 
 
 #preds should have shape (3140,2) with predicted dem_percent, and predicted turnout
 
 
-def electoral_votes(preds):
-    electoral_headers, electoral_votes = read_file("../data/electoral.csv")
+def state_percent(preds):
     headers, data = read_file("../data/full_clean_data.csv")
-    state_percent = np.zeros(51)
-    for i in range(len(state_percent)):
-        state_name = electoral_votes[i,0]
+    electoral_headers, electorate = read_file("../data/electoral.csv")
+    percent = np.zeros(51)
+    for i in range(len(percent)):
+        state_name = electorate[i,0]
         counties = data[state_name == data[:,2]]
         state_preds = preds[state_name == data[:,2]]
         dems = (state_preds[:,0] * state_preds[:,1] * counties[:,3].astype(float)).sum()
         total = (state_preds[:,1] * counties[:,3].astype(float)).sum()
-        state_percent[i] = dems/total
-    dem_electorates = np.around(np.around(state_percent) * electoral_votes[:,1].astype(int)).sum()
-    return 538 - dem_electorates, dem_electorates
+        percent[i] = dems/total
+    return percent
 
-#incomplete, might want later
-'''
-def votes_off(preds):
+def electoral_votes(preds):
     electoral_headers, electoral_votes = read_file("../data/electoral.csv")
-    headers, data = read_file("../data/full_clean_data.csv")
-    state_percent = np.zeros(51)
-    diff = [0, 0]
-    for i in range(len(state_percent)):
-        state_name = electoral_votes[i,0]
-        counties = data[state_name == data[:,2]]
-        state_preds = preds[state_name == data[:,2]]
-        dems_pred = (state_preds[:,0] * state_preds[:,1] * counties[:,3].astype(float))
-        reps_pred = ((1-state_preds[:,0]) * state_preds[:,1] * counties[:,3].astype(float))
-        reps_actual = counties[:,13].astype(int)
-        dems_actual = counties[:,14].astype(int)
-        print(reps_pred, dems_pred, reps_actual, dems_actual)
-        diff[0] += reps_pred - reps_actual
-        diff[1] += dems_pred - dems_actual
-    return np.around(np.array(diff))
-'''
+    percent = state_percent(preds)
+    print(percent)
+    dem_electorates = np.around(np.around(percent) * electoral_votes[:,1].astype(int)).sum()
+    return 538 - dem_electorates, dem_electorates
