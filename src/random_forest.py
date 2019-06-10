8f7330f2596138b429dbba59a195246b9a5f07e6
Raymond Gilmartin <Raymond@DN51vk1o.SUNet>
2019-06-10 14:04:07 -0700

    Added forest_train_predict method to random_forest.py

--- src/random_forest.py
+++ src/random_forest.py
@@ -1,15 +1,14 @@
 import numpy as np
 import pandas as pd
-from util import add_noise, write_file, read_file, county_state_matrix, basic_data
+from util import plot, add_noise, write_file, read_file, county_state_matrix, basic_data
 from sklearn.ensemble import RandomForestRegressor
+import results
 
 # Fit a random forest model, without using active learning.
 # Returns: The fitted model.
 # pct_polled: The proportion of data randomly chosen to be revealed.
 #           Should be a decimal between 0 and 1. Defaults to 1.
 def baseline(pct_polled=1):
-    np.random.seed(1)
-
     x, y = basic_data()
     n_counties = x.shape[0]
     n_train = int(np.round(n_counties * pct_polled))
@@ -17,20 +16,58 @@ def baseline(pct_polled=1):
     x_train = x[train_subset, :]
     y_train = add_noise(y[train_subset, :])
 
-    forest = RandomForestRegressor(n_estimators=100, max_depth=4,
-                                   random_state=2, )
+    forest = RandomForestRegressor(n_estimators=50, max_depth=4,
+                                   random_state=2)
     forest.fit(x_train, y_train)
     return forest
 
+def forest_train_predict(X, X_train, y_train):
+    forest = RandomForestRegressor(n_estimators=50, max_depth=4,
+                                   random_state=2)
+    forest.fit(X_train, y_train)
+    preds = forest.predict(X)
+    return preds
 
-def predict_all_counties(forest, save_path):
+def predict_and_record(forest, save_path):
+    x, y = basic_data()
     predictions = forest.predict(x)
-    sq_err = np.sqrt(np.square(predictions - y))
+    sq_err = np.square(predictions - y)
     full_x = read_file('../data/full_clean_data.csv')
-    data = np.concatenate((full_x[0][:, :3], predictions, sq_err), axis=1)
+    data = np.concatenate((full_x[1][:, :3], predictions, sq_err), axis=1)
     header = full_x[0][:7]
-    header[3] = "pred_turnout"
-    header[4] = "pred_dem"
-    header[5] = "sq_err_turnout"
-    header[6] = "sq_err_dem"
+    header[3] = "pred_dem"
+    header[4] = "pred_turnout"
+    header[5] = "sq_err_dem"
+    header[6] = "sq_err_turnout"
     write_file(save_path, header, data)
+
+
+def main():
+    np.random.seed(1)
+    mse = np.zeros((10, 2))
+    x, y = basic_data()
+    percentages = [1, 2, 3, 4, 5, 10, 20, 40, 60, 100]
+
+    for i in percentages:
+        pct_polled = i/100
+        forest = baseline(pct_polled)
+        save_path = "output/random_forest/baseline_" + str(i) + ".csv"
+        predict_and_record(forest, save_path)
+        mse[i-1:] = np.mean(np.square(forest.predict(x) - y), axis=0)
+        print(i, "% trial complete")
+
+    percentages = np.array(percentages).reshape((10, 1))
+    data = np.concatenate((percentages, mse), axis=1)
+    write_file("output/random_forest/baseline_mse.csv",
+               ["percent_counties_polled", "mse_dem", "mse_turnout"], data)
+
+    plot("/random_forest/mse_plot_50_4.png",
+         percentages.reshape(10,),
+         np.transpose(mse),
+         legend=["MSE of % Democrat", "MSE of % Turnout"],
+         x_label="Percent of Counties Polled",
+         y_label="Mean Squared Error",
+         title="MSE for Random Forest Baseline")
+
+
+if __name__ == "__main__": main()
